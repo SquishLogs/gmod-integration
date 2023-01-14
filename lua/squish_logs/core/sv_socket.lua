764733eca661c643e32490aa4854b7e87fb86775
Owain <owain.jones74@gmail.com>
2023-01-14 15:45:53 +0000

    use improved auth method

--- lua/squish_logs/core/sv_socket.lua
+++ lua/squish_logs/core/sv_socket.lua
@@ -1,5 +1,7 @@
 require("gwsockets")
 
+
+local firstConnect = false
 function SquishLogs.Socket.StartConnection()
 	print("[Squish Logs]", "Starting websocket connection")
 
@@ -10,6 +12,19 @@ function SquishLogs.Socket.StartConnection()
 	SquishLogs.Socket.Connection = GWSockets.createWebSocket(SquishLogs.Server.socket.ip_address, false)
 
 	function SquishLogs.Socket.Connection:onMessage(txt)
+		// They're reconnecting on an existing session
+		if (firstConnect) then return end
+		firstConnect = true // This is to prevent the archive from triggering again if connection was lost
+		
+		// Receive a response informing us the auth was successful
+		local response = util.JSONToTable(txt)
+
+		if (!response) then return end
+		if (!response.type or !(response.type == 'auth-response')) then return end
+		if (!response.success) then return end
+
+		SquishLogs.Core.Archive()
+		SquishLogs.Core.SimpleLog("Server started")
 	end
 	
 	function SquishLogs.Socket.Connection:onError(txt)
@@ -23,9 +38,6 @@ function SquishLogs.Socket.StartConnection()
 			community = SquishLogs.Info.Community,
 			token = SquishLogs.Info.Token
 		})
-
-		SquishLogs.Core.Archive()
-		SquishLogs.Core.SimpleLog("Server started")
 	end
 	function SquishLogs.Socket.Connection:onDisconnected()
 		print("[Squish Logs]", "WebSocket disconnected")
