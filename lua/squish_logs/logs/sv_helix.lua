8a4b1dfe0371ffbeca9533a8199e54210d6901d9
Higamato <sametcamur10@hotmail.com>
2023-01-25 02:47:52 +0300

    Helix integration

---
+++ lua/squish_logs/logs/sv_helix.lua
@@ -0,0 +1,26 @@
+local colorYellow = Color(146,138,64)
+local colorGreen = Color(34,139,34)
+local colorRed = Color(200, 0, 0)
+local colorBlue = Color(29, 97, 129)
+
+--Its just sends the helix's default logs to the Site
+
+hook.Add("PostLoadData", "Squish_Logs_Helix_Int", function()
+
+    local HANDLER = {}
+
+    function HANDLER.Load()
+        print("Helix Squish_Logs is Started")
+    end
+
+    function HANDLER.Write(client, message)
+        SquishLog:New()
+        :SetCategory("Helix")
+        :AddFragment(client)
+        :AddFragment( " " .. message)
+        :Send()
+    end
+
+    ix.log.RegisterHandler("Squish_Logs", HANDLER)
+
+end)
