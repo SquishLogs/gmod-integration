07e2abc19ee16b3aa90f386c74fa4b1afa5bca5e
Higamato <sametcamur10@hotmail.com>
2023-01-25 15:56:47 +0300

    Startup crash fix

--- lua/squish_logs/logs/sv_helix.lua
+++ lua/squish_logs/logs/sv_helix.lua
@@ -3,20 +3,22 @@ local colorGreen = Color(34,139,34)
 local colorRed = Color(200, 0, 0)
 local colorBlue = Color(29, 97, 129)
 
---Its just sends the helix's default logs to the Site
-
 hook.Add("PostLoadData", "Squish_Logs_Helix_Int", function()
 
     local HANDLER = {}
 
     function HANDLER.Write(client, message, logFlag, logType)
+        local chartxt = ""
+        if IsValid(client:GetCharacter()) then
+            chartxt = " CharID ".. client:GetCharacter():GetID()
+        end
 
         SquishLog:New()
         :SetCategory("Helix | " .. logType)
         :AddFragment(client)
         :AddFragment( " " .. string.gsub(message, client:Nick(), ""))
         :SetFragmentColor(colorYellow)
-        :AddFragment( " CharID " .. client:GetCharacter():GetID())
+        :AddFragment( chartxt )
         :Send()
     end
 
