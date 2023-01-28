e621b183db1a857daaf5f00a2081c7847075d410
Owain <owain.jones74@gmail.com>
2023-01-28 22:12:53 +0000

    dont connect on send

--- lua/squish_logs/classes/sv_log.lua
+++ lua/squish_logs/classes/sv_log.lua
@@ -81,10 +81,6 @@ end
 
 // Send the log
 function SquishLog:Send()
-    // Connection timed out or something, reconnect.
-    if (!SquishLogs.Socket.Connection) then SquishLogs.Socket.StartConnection() end
-    
-    // Send off the log
     SquishLogs.Socket.Send({
         type = "log",
         category = self.category,
