9b19975d542eefc84f84ef1c830bf7066bd8183a
Livaco <livaco@livaco.dev>
2023-02-03 08:23:55 +0000

    change to ipairs in the autoloader

--- lua/autorun/squish_logs_init.lua
+++ lua/autorun/squish_logs_init.lua
@@ -3,7 +3,7 @@ SquishLogs.Info = {}
 SquishLogs.Socket = {}
 SquishLogs.Core = {}
 
-SquishLogs.Info.Version = "1.0.5"
+SquishLogs.Info.Version = "1.0.6"
 SquishLogs.Info.Domain = "{{ community_domain }}"
 
 print("Loading Squish Logs")
@@ -12,37 +12,35 @@ local path = "squish_logs/"
 if (SERVER) then
 	local files, folders = file.Find(path .. "*", "LUA")
 
-	for _, folder in SortedPairs(folders, true) do
+	for _, folder in ipairs(folders) do
 		print("Loading folder:", folder)
-	    for b, File in SortedPairs(file.Find(path .. folder .. "/sh_*.lua", "LUA"), true) do
+	    for b, File in ipairs(file.Find(path .. folder .. "/sh_*.lua", "LUA")) do
 	    	print("Loading file:", File)
 	        AddCSLuaFile(path .. folder .. "/" .. File)
 	        include(path .. folder .. "/" .. File)
 	    end
 
-	    for b, File in SortedPairs(file.Find(path .. folder .. "/sv_*.lua", "LUA"), true) do
+	    for b, File in ipairs(file.Find(path .. folder .. "/sv_*.lua", "LUA")) do
 	    	print("Loading file:", File)
 	        include(path .. folder .. "/" .. File)
 	    end
 
-	    for b, File in SortedPairs(file.Find(path .. folder .. "/cl_*.lua", "LUA"), true) do
+	    for b, File in ipairs(file.Find(path .. folder .. "/cl_*.lua", "LUA")) do
 	    	print("Loading file:", File)
 	        AddCSLuaFile(path .. folder .. "/" .. File)
 	    end
 	end
-end
-
-if CLIENT then
+else
 	local files, folders = file.Find(path .. "*", "LUA")
 
-	for _, folder in SortedPairs(folders, true) do
+	for _, folder in ipairs(folders) do
 		print("Loading folder:", folder)
-	    for b, File in SortedPairs(file.Find(path .. folder .. "/sh_*.lua", "LUA"), true) do
+	    for b, File in ipairs(file.Find(path .. folder .. "/sh_*.lua", "LUA")) do
 	    	print("Loading file:", File)
 	        include(path .. folder .. "/" .. File)
 	    end
 
-	    for b, File in SortedPairs(file.Find(path .. folder .. "/cl_*.lua", "LUA"), true) do
+	    for b, File in ipairs(file.Find(path .. folder .. "/cl_*.lua", "LUA")) do
 	    	print("Loading file:", File)
 	        include(path .. folder .. "/" .. File)
 	    end
