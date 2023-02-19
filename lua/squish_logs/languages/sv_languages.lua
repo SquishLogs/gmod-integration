e1c7af41e3f79f211f19e24c24f4db3d4fecba4c
Hendalf <56977415+Hendalf-DEV@users.noreply.github.com>
2023-02-19 16:05:16 +0200

    Language System Change to JSON fetch

--- lua/squish_logs/languages/sv_languages.lua
+++
@@ -1,9 +0,0 @@
-local lang = include(SquishLogs.Language .. ".lua")
-
-function SquishLogs:GetPhrase(phrase)
-    if not lang[phrase] then
-        error("Unknown phrase: " .. phrase)
-        return phrase
-    end
-    return lang[phrase]
-end
