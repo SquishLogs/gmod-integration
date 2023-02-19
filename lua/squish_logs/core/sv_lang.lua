4c223b0df78b19c2f42f3cbb90978c50a8f7868e
Hendalf <56977415+Hendalf-DEV@users.noreply.github.com>
2023-02-19 17:51:26 +0200

    Update sv_lang.lua

--- lua/squish_logs/core/sv_lang.lua
+++ lua/squish_logs/core/sv_lang.lua
@@ -1,16 +1,8 @@
-function SquishLogs:GetPhrase(phrase, callback)
-    if not self.languageData then
-        local url = "https://raw.githubusercontent.com/Hendalf-DEV/Squish-Logs-Lang/main/gmod/" .. self.Language .. ".json"
-        http.Fetch(url, function(body, size, headers, code)
-            self.languageData = util.JSONToTable(body)
-            local result = self.languageData[phrase] or phrase
-            callback(result)
-        end, function(error)
-            print("Failed to fetch language file: " .. error)
-            callback(phrase)
-        end)
+function SquishLogs:GetPhrase(key)
+    local sentence = SquishLogs.Translations[key]
+    if sentence ~= "" then
+        return sentence
     else
-        local result = self.languageData[phrase] or phrase
-        callback(result)
+        return "No translation available for key: " .. key
     end
 end
