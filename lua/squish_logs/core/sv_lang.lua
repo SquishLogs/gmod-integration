d94e122f4f1064b813f69beb1d2f16f030abba25
Hendalf <56977415+Hendalf-DEV@users.noreply.github.com>
2023-02-19 20:12:18 +0200

    Update sv_lang.lua

--- lua/squish_logs/core/sv_lang.lua
+++ lua/squish_logs/core/sv_lang.lua
@@ -6,3 +6,21 @@ function SquishLogs:GetPhrase(key)
         return "No translation available for key: " .. key
     end
 end
+
+local function loadTranslationTable(language)
+    timer.Simple(0, function()
+        http.Fetch("https://raw.githubusercontent.com/Hendalf-DEV/Squish-Logs-Lang/main/gmod/" .. language .. ".json", function(response)
+            if response then
+                SquishLogs.Translations = util.JSONToTable(response)
+                SquishLogs.TranslationsLoaded = true
+            else
+                print("Failed to retrieve translation table for language: " .. language)
+            end
+        end)
+    end)
+end
+
+-- Call load function to load the translation table from the internet if necessary
+if not SquishLogs.TranslationsLoaded then
+    loadTranslationTable(SquishLogs.Language)
+end
